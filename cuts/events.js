cc009d36c24ce6d4c090439aa564acf3a8284d88
Jakob Nanneson <jakob.nanneson@coop.se>
2020-09-25 08:55:55 +0200

    Reusable code snippets

---
+++ cuts/events.js
@@ -0,0 +1,23 @@
+function experimentEvent(action, nonInteraction) {
+  if (typeof ga !== 'undefined') {
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'Experiment',
+      eventAction: action,
+      eventLabel: '',
+      transport: 'beacon',
+      nonInteraction: nonInteraction
+    });
+  } else {
+    console.warn('ga undefined', action);
+  }
+}
+
+experimentEvent('view', true);
+experimentEvent('click-close', false);
+experimentEvent('click-login', false);
+experimentEvent('click-register', false);
+
+if (window.location.pathname == '/') {
+  hj('trigger', 'experiment-loginreminder-variant1');
+}
