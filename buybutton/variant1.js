1271f3a1ca9caf879e576a73d8010df29a351f19
Jakob Nanneson <jakob.nanneson@coop.se>
2020-11-06 20:55:06 +0100

    Adding Buy button

---
+++ buybutton/variant1.js
@@ -0,0 +1,23 @@
+function labelButtons() {
+  console.debug('<experiment> Label buttons');
+  document.querySelectorAll('.js-buttonView:not(.ProductSearch-itemCta)').forEach(function (element) {
+    if (!element.querySelector('.js-input').classList.contains('has-value')) {
+      element.querySelectorAll('.js-kop').forEach(function (element) {
+        element.classList.remove('u-hidden');
+      });
+      element.querySelectorAll('.js-buttonContainer').forEach(function (element) {
+        element.classList.add('u-hidden');
+      });
+    }
+  });
+}
+
+labelButtons();
+document.querySelectorAll('.js-subtract').forEach(function (element) {
+  element.addEventListener('click', function () {
+    window.setTimeout(labelButtons, 1000);
+  });
+});
+window.addEventListener('resize', function () {
+  window.setTimeout(labelButtons, 1000);
+});
---
+++ buybutton/variant2.ts
@@ -0,0 +1,29 @@
+function labelButtons() {
+  console.debug('<experiment> Label buttons');
+  document
+    .querySelectorAll('.js-buttonView:not(.ProductSearch-itemCta)')
+    .forEach((element) => {
+      if (!element.querySelector('.js-input').classList.contains('has-value')) {
+        element.querySelectorAll('.js-kop').forEach((element) => {
+          element.classList.remove('u-hidden');
+          element.style.textOverflow = 'unset';
+          element.textContent = 'Lägg till';
+        });
+        element.querySelectorAll('.js-buttonContainer').forEach((element) => {
+          element.classList.add('u-hidden');
+        });
+      }
+    });
+}
+
+labelButtons();
+
+document.querySelectorAll('.js-subtract').forEach((element) => {
+  element.addEventListener('click', () => {
+    window.setTimeout(labelButtons, 1000);
+  });
+});
+
+window.addEventListener('resize', () => {
+  window.setTimeout(labelButtons, 1000);
+});
