c4c8251ad8cef9489046094b7dec45d3d0ea87ae
Jakob Nanneson <jakob.nanneson@coop.se>
2021-01-28 14:19:12 +0100

    Add tracking to link

--- onlineoffers/variant1.js
+++ onlineoffers/variant1.js
@@ -2,10 +2,25 @@ var storeModulesSection = document.querySelector('.js-storeModulesSection');
 
 if (storeModulesSection) {
   var onlineoffersStyle = document.createElement('style');
-  onlineoffersStyle.innerHTML = "\n        .experiment.onlineoffers {\n            box-sizing: border-box;\n            text-align: center;\n            background-color: #e8f2d2;\n            padding: 13px 16px;\n            width: 100%;\n            margin: 1em 0;\n        }\n        .experiment.onlineoffers a {\n            margin-top: 8px;\n        }";
+  onlineoffersStyle.innerHTML = "\n    .experiment.onlineoffers {\n      box-sizing: border-box;\n      text-align: center;\n      background-color: #e8f2d2;\n      padding: 13px 16px;\n      width: 100%;\n      margin: 1em 0;\n    }\n    .experiment.onlineoffers a {\n      margin-top: 8px;\n    }";
   storeModulesSection.appendChild(onlineoffersStyle);
   var onlineoffers = document.createElement('div');
-  onlineoffers.classList.add('Grid-cell', 'u-sizeFull');
-  onlineoffers.innerHTML = "\n        <div class=\"experiment onlineoffers\">\n            <div>Det finns ytterligare erbjudanden n\xE4r du best\xE4ller fr\xE5n coop.se</div>\n            <div><a href=\"/handla/aktuella-erbjudanden/\" class=\"Button Button--green Button--small Button--radius\">Aktuella erbjudanden</a></div>\n        </div>";
+  onlineoffers.classList.add('experiment', 'onlineoffers');
+  onlineoffers.innerHTML = "\n    <div>Denna sida visar erbjudanden i butik. Det finns andra erbjudanden n\xE4r du best\xE4ller fr\xE5n coop.se</div>";
+  var onlineoffersLink = document.createElement('a');
+  onlineoffersLink.classList.add('Button', 'Button--green', 'Button--small', 'Button--radius');
+  onlineoffersLink.innerText = 'Visa';
+  onlineoffersLink.addEventListener('click', function (event) {
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'Experiment',
+      eventAction: 'online-offers-click',
+      eventLabel: '',
+      transport: 'beacon',
+      nonInteraction: false
+    });
+    document.location.href = '/handla/aktuella-erbjudanden/';
+  });
+  onlineoffers.appendChild(onlineoffersLink);
   storeModulesSection.appendChild(onlineoffers);
 }
--- onlineoffers/variant1.ts
+++ onlineoffers/variant1.ts
@@ -3,25 +3,45 @@ let storeModulesSection = document.querySelector('.js-storeModulesSection');
 if (storeModulesSection) {
   const onlineoffersStyle = document.createElement('style');
   onlineoffersStyle.innerHTML = `
-        .experiment.onlineoffers {
-            box-sizing: border-box;
-            text-align: center;
-            background-color: #e8f2d2;
-            padding: 13px 16px;
-            width: 100%;
-            margin: 1em 0;
-        }
-        .experiment.onlineoffers a {
-            margin-top: 8px;
-        }`;
+    .experiment.onlineoffers {
+      box-sizing: border-box;
+      text-align: center;
+      background-color: #e8f2d2;
+      padding: 13px 16px;
+      width: 100%;
+      margin: 1em 0;
+    }
+    .experiment.onlineoffers a {
+      margin-top: 8px;
+    }`;
   storeModulesSection.appendChild(onlineoffersStyle);
 
   const onlineoffers = document.createElement('div');
-  onlineoffers.classList.add('Grid-cell', 'u-sizeFull');
+  onlineoffers.classList.add('experiment', 'onlineoffers');
   onlineoffers.innerHTML = `
-        <div class="experiment onlineoffers">
-            <div>Det finns ytterligare erbjudanden när du beställer från coop.se</div>
-            <div><a href="/handla/aktuella-erbjudanden/" class="Button Button--green Button--small Button--radius">Aktuella erbjudanden</a></div>
-        </div>`;
+    <div>Denna sida visar erbjudanden i butik. Det finns andra erbjudanden när du beställer från coop.se</div>`;
+
+  const onlineoffersLink = document.createElement('a');
+  onlineoffersLink.classList.add(
+    'Button',
+    'Button--green',
+    'Button--small',
+    'Button--radius'
+  );
+  onlineoffersLink.innerText = 'Visa';
+  onlineoffersLink.addEventListener('click', (event) => {
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'Experiment',
+      eventAction: 'online-offers-click',
+      eventLabel: '',
+      transport: 'beacon',
+      nonInteraction: false,
+    });
+    document.location.href = '/handla/aktuella-erbjudanden/';
+  });
+
+  onlineoffers.appendChild(onlineoffersLink);
+
   storeModulesSection.appendChild(onlineoffers);
 }
