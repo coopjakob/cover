3dfedd729c457580f3c20e74bd26d94bd426dc0a
Jakob Nanneson <jakob.nanneson@coop.se>
2021-02-04 09:10:52 +0100

    Removed style tag and used utility classes

--- onlineoffers/variant1.js
+++ onlineoffers/variant1.js
@@ -1,23 +1,17 @@
 var storeModulesSection = document.querySelector('.js-storeModulesSection');
 
 if (storeModulesSection) {
-  var onlineoffersStyle = document.createElement('style');
-  onlineoffersStyle.innerHTML = "\n    .experiment.onlineoffers {\n      box-sizing: border-box;\n      text-align: center;\n      background-color: #e8f2d2;\n      padding: 13px 16px;\n      width: 100%;\n      margin: 1em 0;\n    }\n    .experiment.onlineoffers a {\n      margin-top: 8px;\n    }";
-  storeModulesSection.appendChild(onlineoffersStyle);
   var onlineoffers = document.createElement('div');
-  onlineoffers.classList.add('experiment', 'onlineoffers');
-  onlineoffers.innerHTML = "\n    <div>Sidan visar erbjudanden i butik. Det finns andra <a href=\"/handla/aktuella-erbjudanden/\">erbjudanden</a> om du best\xE4ller fr\xE5n coop.se</div>";
+  onlineoffers.classList.add('u-textCenter', 'u-paddingAmd', 'u-bgGreenLight2');
+  onlineoffers.innerHTML = "\n    Sidan visar erbjudanden i butik. Det finns andra <a href=\"/handla/aktuella-erbjudanden/\">erbjudanden</a> om du best\xE4ller fr\xE5n coop.se";
   var onlineoffersLink = onlineoffers.querySelector('a');
   onlineoffersLink.classList.add('Link', 'Link--green');
-  onlineoffersLink.style.marginTop = '0px';
   onlineoffersLink.addEventListener('click', function (event) {
     ga('send', {
       hitType: 'event',
       eventCategory: 'Experiment',
       eventAction: 'online-offers-click',
-      eventLabel: '',
-      transport: 'beacon',
-      nonInteraction: false
+      transport: 'beacon'
     });
   });
   storeModulesSection.appendChild(onlineoffers);
--- onlineoffers/variant1.ts
+++ onlineoffers/variant1.ts
@@ -1,37 +1,19 @@
 let storeModulesSection = document.querySelector('.js-storeModulesSection');
 
 if (storeModulesSection) {
-  const onlineoffersStyle = document.createElement('style');
-  onlineoffersStyle.innerHTML = `
-    .experiment.onlineoffers {
-      box-sizing: border-box;
-      text-align: center;
-      background-color: #e8f2d2;
-      padding: 13px 16px;
-      width: 100%;
-      margin: 1em 0;
-    }
-    .experiment.onlineoffers a {
-      margin-top: 8px;
-    }`;
-  storeModulesSection.appendChild(onlineoffersStyle);
-
   const onlineoffers = document.createElement('div');
-  onlineoffers.classList.add('experiment', 'onlineoffers');
+  onlineoffers.classList.add('u-textCenter', 'u-paddingAmd', 'u-bgGreenLight2');
   onlineoffers.innerHTML = `
-    <div>Sidan visar erbjudanden i butik. Det finns andra <a href="/handla/aktuella-erbjudanden/">erbjudanden</a> om du beställer från coop.se</div>`;
+    Sidan visar erbjudanden i butik. Det finns andra <a href="/handla/aktuella-erbjudanden/">erbjudanden</a> om du beställer från coop.se`;
 
   const onlineoffersLink = onlineoffers.querySelector('a');
   onlineoffersLink.classList.add('Link', 'Link--green');
-  onlineoffersLink.style.marginTop = '0px';
   onlineoffersLink.addEventListener('click', (event) => {
     ga('send', {
       hitType: 'event',
       eventCategory: 'Experiment',
       eventAction: 'online-offers-click',
-      eventLabel: '',
       transport: 'beacon',
-      nonInteraction: false,
     });
   });
 
