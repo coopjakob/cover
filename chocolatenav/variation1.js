44a31b2936bee87d0717ec972f1c0f46abe4c3d2
Jakob Nanneson <jakob.nanneson@coop.se>
2021-02-04 15:16:42 +0100

    Change navigation icon to 3x3 squares

---
+++ chocolatenav/variation1.js
@@ -0,0 +1,5 @@
+var sidebarTriggerBarIcon = document.querySelector('.js-sidebarTrigger .Bar-icon');
+
+if (sidebarTriggerBarIcon) {
+  sidebarTriggerBarIcon.innerHTML = "\n    <svg width=\"16\" height=\"16\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\">\n      <title>Navigation</title>\n      <path fill=\"black\" d=\"m0,4l4,0l0,-4l-4,0l0,4zm6,12l4,0l0,-4l-4,0l0,4zm-6,0l4,0l0,-4l-4,0l0,4zm0,-6l4,0l0,-4l-4,0l0,4zm6,0l4,0l0,-4l-4,0l0,4zm6,-10l0,4l4,0l0,-4l-4,0zm-6,4l4,0l0,-4l-4,0l0,4zm6,6l4,0l0,-4l-4,0l0,4zm0,6l4,0l0,-4l-4,0l0,4z\"/>\n    </svg>";
+}
