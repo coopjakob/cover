87a746aea363c5b29ee218b99202aa049479708d
Jakob Nanneson <jakob.nanneson@coop.se>
2021-02-05 09:54:31 +0100

    Show subcategories on category pages

---
+++ subcategories/variant1.ts
@@ -0,0 +1,49 @@
+let experimentArea = document.createElement('div');
+experimentArea.classList.add('experiment', 'u-marginBmd');
+
+let sidebarNavItemExpanded = document.querySelectorAll(
+  '.js-sidebarNavItem.has-children.is-expanded'
+);
+
+let inTopLevel = sidebarNavItemExpanded.length == 1;
+
+let headingTitle;
+if (inTopLevel) {
+  headingTitle = sidebarNavItemExpanded[0].querySelectorAll(
+    '.js-sectionSubLevel .SidebarNav-headingTitle'
+  );
+} else {
+  headingTitle = sidebarNavItemExpanded[1].querySelectorAll(
+    '.SidebarNav-subPanel .SidebarNav-headingTitle'
+  );
+}
+
+headingTitle.forEach((title) => {
+  let link = document.createElement('a');
+  link.classList.add(
+    'js-asyncLink',
+    'js-link',
+    'Link',
+    'Link--green',
+    'u-paddingAxsm',
+    'u-marginRsm',
+    'u-marginVxxsm',
+    'u-bgWhite'
+  );
+  link.style.display = 'inline-block';
+  link.setAttribute('href', title.parentElement.getAttribute('href'));
+  link.textContent = title.textContent;
+  link.addEventListener('click', (event) => {
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'Experiment',
+      eventAction: 'subcategories-click',
+      eventLabel: 'level ' + sidebarNavItemExpanded.length + 1,
+      transport: 'beacon',
+    });
+  });
+  experimentArea.append(link);
+});
+
+let productListParent = document.querySelector('.js-productList').parentElement;
+productListParent.prepend(experimentArea);
