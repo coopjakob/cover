886e304fae86747aa9bb39f968e4b943555c3d97
Jakob Nanneson <jakob.nanneson@coop.se>
2021-03-01 22:32:15 +0100

    Check if navigation is updated with current page

--- subcategories/variant1.js
+++ subcategories/variant1.js
@@ -1,33 +1,54 @@
-var experimentArea = document.createElement('div');
-experimentArea.classList.add('experiment', 'u-marginBsm');
-var sidebarNavItemExpanded = document.querySelectorAll('.js-sidebarNavItem.is-expanded');
+function showSubcategories() {
+  var sidebarNavItemExpanded = document.querySelectorAll('.js-sidebarNavItem.is-expanded');
+  console.debug('sidebarNavItemExpanded:', sidebarNavItemExpanded);
+  var experimentArea = document.createElement('div');
+  experimentArea.classList.add('experiment', 'u-marginBsm');
 
-if (sidebarNavItemExpanded.length <= 2) {
-  var inTopLevel = sidebarNavItemExpanded.length == 1;
-  var headingTitle;
+  if (sidebarNavItemExpanded.length <= 2) {
+    var inTopLevel = sidebarNavItemExpanded.length == 1;
+    var headingTitle;
 
-  if (inTopLevel) {
-    headingTitle = sidebarNavItemExpanded[0].querySelectorAll('.js-sectionSubLevel .SidebarNav-headingTitle');
-  } else {
-    headingTitle = sidebarNavItemExpanded[1].querySelectorAll('.SidebarNav-subPanel .SidebarNav-headingTitle');
-  }
+    if (inTopLevel) {
+      headingTitle = sidebarNavItemExpanded[0].querySelectorAll('.js-sectionSubLevel .SidebarNav-headingTitle');
+    } else {
+      headingTitle = sidebarNavItemExpanded[1].querySelectorAll('.SidebarNav-subPanel .SidebarNav-headingTitle');
+    }
 
-  headingTitle.forEach(function (title) {
-    var link = document.createElement('a');
-    link.classList.add('js-asyncLink', 'js-link', 'Link', 'Link--green', 'u-paddingAxsm', 'u-marginRxxsm', 'u-marginBxxsm', 'u-bgWhite');
-    link.style.display = 'inline-block';
-    link.setAttribute('href', title.parentElement.getAttribute('href'));
-    link.textContent = title.textContent;
-    link.addEventListener('click', function (event) {
-      dataLayer.push({
-        event: 'interaction',
-        eventCategory: 'Experiment',
-        eventAction: 'subcategories-click',
-        eventLabel: 'level ' + sidebarNavItemExpanded.length
+    headingTitle.forEach(function (title) {
+      var link = document.createElement('a');
+      link.classList.add('js-asyncLink', 'js-link', 'Link', 'Link--green', 'u-paddingAxsm', 'u-marginRxxsm', 'u-marginBxxsm', 'u-bgWhite');
+      link.style.display = 'inline-block';
+      link.setAttribute('href', title.parentElement.getAttribute('href'));
+      link.textContent = title.textContent;
+      link.addEventListener('click', function (event) {
+        dataLayer.push({
+          event: 'interaction',
+          eventCategory: 'Experiment',
+          eventAction: 'subcategories-click',
+          eventLabel: 'level ' + sidebarNavItemExpanded.length
+        });
       });
+      experimentArea.append(link);
     });
-    experimentArea.append(link);
-  });
-  var productListParent = document.querySelector('.js-productList').parentElement;
-  productListParent.prepend(experimentArea);
+    var productListParent = document.querySelector('.js-productList').parentElement;
+    productListParent.prepend(experimentArea);
+  }
 }
+
+function isMenuUpdated() {
+  console.debug('<experiment> check if menu is updated');
+  var allExpanded = document.querySelectorAll('.js-sidebarNavItem.is-expanded');
+  var lastExpanded = allExpanded[allExpanded.length - 1];
+  var sectionHeader = lastExpanded.firstElementChild;
+  var sectionPath = sectionHeader.firstElementChild.getAttribute('href');
+
+  if (sectionPath == window.location.pathname) {
+    console.debug('<experiment> menu updated, show subcategories');
+    showSubcategories();
+  } else {
+    console.debug('<experiment> old menu, try again');
+    setTimeout(isMenuUpdated, 100);
+  }
+}
+
+isMenuUpdated();
--- subcategories/variant1.ts
+++ subcategories/variant1.ts
@@ -1,51 +1,72 @@
-let experimentArea = document.createElement('div');
-experimentArea.classList.add('experiment', 'u-marginBsm');
+function showSubcategories() {
+  let sidebarNavItemExpanded = document.querySelectorAll(
+    '.js-sidebarNavItem.is-expanded'
+  );
+  console.debug('sidebarNavItemExpanded:', sidebarNavItemExpanded);
 
-let sidebarNavItemExpanded = document.querySelectorAll(
-  '.js-sidebarNavItem.is-expanded'
-);
+  let experimentArea = document.createElement('div');
+  experimentArea.classList.add('experiment', 'u-marginBsm');
 
-if (sidebarNavItemExpanded.length <= 2) {
-  let inTopLevel = sidebarNavItemExpanded.length == 1;
+  if (sidebarNavItemExpanded.length <= 2) {
+    let inTopLevel = sidebarNavItemExpanded.length == 1;
 
-  let headingTitle;
-  if (inTopLevel) {
-    headingTitle = sidebarNavItemExpanded[0].querySelectorAll(
-      '.js-sectionSubLevel .SidebarNav-headingTitle'
-    );
-  } else {
-    headingTitle = sidebarNavItemExpanded[1].querySelectorAll(
-      '.SidebarNav-subPanel .SidebarNav-headingTitle'
-    );
-  }
+    let headingTitle;
+    if (inTopLevel) {
+      headingTitle = sidebarNavItemExpanded[0].querySelectorAll(
+        '.js-sectionSubLevel .SidebarNav-headingTitle'
+      );
+    } else {
+      headingTitle = sidebarNavItemExpanded[1].querySelectorAll(
+        '.SidebarNav-subPanel .SidebarNav-headingTitle'
+      );
+    }
 
-  headingTitle.forEach((title) => {
-    let link = document.createElement('a');
-    link.classList.add(
-      'js-asyncLink',
-      'js-link',
-      'Link',
-      'Link--green',
-      'u-paddingAxsm',
-      'u-marginRxxsm',
-      'u-marginBxxsm',
-      'u-bgWhite'
-    );
-    link.style.display = 'inline-block';
-    link.setAttribute('href', title.parentElement.getAttribute('href'));
-    link.textContent = title.textContent;
-    link.addEventListener('click', (event) => {
-      dataLayer.push({
-        event: 'interaction',
-        eventCategory: 'Experiment',
-        eventAction: 'subcategories-click',
-        eventLabel: 'level ' + sidebarNavItemExpanded.length,
+    headingTitle.forEach((title) => {
+      let link = document.createElement('a');
+      link.classList.add(
+        'js-asyncLink',
+        'js-link',
+        'Link',
+        'Link--green',
+        'u-paddingAxsm',
+        'u-marginRxxsm',
+        'u-marginBxxsm',
+        'u-bgWhite'
+      );
+      link.style.display = 'inline-block';
+      link.setAttribute('href', title.parentElement.getAttribute('href'));
+      link.textContent = title.textContent;
+      link.addEventListener('click', (event) => {
+        dataLayer.push({
+          event: 'interaction',
+          eventCategory: 'Experiment',
+          eventAction: 'subcategories-click',
+          eventLabel: 'level ' + sidebarNavItemExpanded.length,
+        });
       });
+      experimentArea.append(link);
     });
-    experimentArea.append(link);
-  });
 
-  let productListParent = document.querySelector('.js-productList')
-    .parentElement;
-  productListParent.prepend(experimentArea);
+    let productListParent = document.querySelector('.js-productList')
+      .parentElement;
+    productListParent.prepend(experimentArea);
+  }
 }
+
+function isMenuUpdated() {
+  console.debug('<experiment> check if menu is updated');
+
+  let allExpanded = document.querySelectorAll('.js-sidebarNavItem.is-expanded');
+  let lastExpanded = allExpanded[allExpanded.length - 1];
+  let sectionHeader = lastExpanded.firstElementChild;
+  let sectionPath = sectionHeader.firstElementChild.getAttribute('href');
+
+  if (sectionPath == window.location.pathname) {
+    console.debug('<experiment> menu updated, show subcategories');
+    showSubcategories();
+  } else {
+    console.debug('<experiment> old menu, try again');
+    setTimeout(isMenuUpdated, 100);
+  }
+}
+isMenuUpdated();
