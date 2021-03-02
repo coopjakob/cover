cb38e6e910ee3aa791ac4df9d99a4a191251d20e
Jakob Nanneson <jakob.nanneson@coop.se>
2021-03-02 09:03:11 +0100

    Use js instead of html when creating elements

--- headercheckout/variant1.ts
+++ headercheckout/variant1.ts
@@ -1,32 +1,22 @@
-let element = document.createElement('li');
-element.classList.add(
+let container = document.createElement('li');
+container.classList.add(
   'experiment',
   't43',
   'Navigation-item',
   'u-paddingLmd',
   'u-paddingRz'
 );
-element.innerHTML = `
-  <div class="Button Button--green Button--radius u-paddingHsm ">
-    <a href="https://www.coop.se/handla/betala/" style="color: white!important">Till kassan</a>
-  </div>
-`;
 
-let wrapper = document.querySelector(
-  '.Navigation--secondaryGreen .Navigation-list'
+let button = document.createElement('div');
+button.classList.add(
+  'Button',
+  'Button--green',
+  'Button--radius',
+  'u-paddingHsm'
 );
-wrapper.append(element);
-
-let style = document.createElement('style');
-style.innerHTML = `
-  .t43 .Button--green:hover {
-    border-color: white;
-  }
-`;
-document.body.appendChild(style);
 
 let link = document.createElement('a');
-link.style.color = 'white!important';
+link.style.color = 'white';
 link.textContent = 'Till kassan';
 link.addEventListener('click', (event) => {
   dataLayer.push({
@@ -34,10 +24,22 @@ link.addEventListener('click', (event) => {
     eventCategory: 'Experiment',
     eventAction: 'headercheckout-click',
   });
-
-  if (window.dataLayer.find((x) => x.event === 'nonInteraction').storeId == 0) {
-    document.querySelector('.js-cncTrigger .CartButton').click();
-  } else {
-    location.href = 'https://www.coop.se/handla/betala/';
-  }
+  location.href = 'https://www.coop.se/handla/betala/';
 });
+
+let wrapper = document.querySelector(
+  '.Navigation--secondaryGreen .Navigation-list'
+);
+
+button.append(link);
+container.append(button);
+wrapper.append(container);
+
+let style = document.createElement('style');
+style.innerHTML = `
+  .t43 .Button--green:hover {
+    background-color: unset;
+    border-color: white;
+  }
+`;
+document.body.appendChild(style);
