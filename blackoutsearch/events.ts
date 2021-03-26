ca0f7becdbc30caa7b411da8c9670b70ac1c2688
Jakob Nanneson <jakob.nanneson@coop.se>
2021-03-26 11:24:34 +0100

    Global events to identify when search is cancelled

---
+++ blackoutsearch/events.ts
@@ -0,0 +1,41 @@
+document
+  .querySelector('.Search-input')
+  .addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      console.debug('<experiment> escapekey');
+      dataLayer.push({
+        event: 'interaction',
+        eventCategory: 'Experiment',
+        eventAction: 'search-escapekey',
+        eventLabel: '',
+      });
+    }
+  });
+
+const observer = new MutationObserver((mutations) => {
+  console.debug('<experiment> change detected');
+  for (const { addedNodes } of mutations) {
+    for (const node of addedNodes) {
+      if (!node.tagName) continue; // not an element
+      if (node.classList.contains('Search-clear')) {
+        node.addEventListener('click', () => {
+          console.debug('<experiment> closeicon');
+          dataLayer.push({
+            event: 'interaction',
+            eventCategory: 'Experiment',
+            eventAction: 'search-closeicon',
+            eventLabel: '',
+          });
+        });
+      }
+    }
+  }
+});
+
+let wrapper = document.querySelector('.Search-content');
+
+console.debug('<experiment> observing search');
+observer.observe(wrapper, {
+  childList: true,
+  subtree: false,
+});
