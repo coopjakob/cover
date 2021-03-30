bc0f97c9e203f2e0ed16af3c09a3ec6a067ac299
Jakob Nanneson <jakob.nanneson@coop.se>
2021-03-30 09:49:40 +0200

    Only trigger if disabled button exist

--- drawercheckout/trigger.js
+++ drawercheckout/trigger.js
@@ -4,7 +4,9 @@ eventAction: 'Slider - Slider öppnas'; // eventCategory: 'Ecommerce';
 // eventLabel: 'Cart ikon';
 
 
-console.debug('<experiment> activate drawercheckout');
-dataLayer.push({
-  event: 'optimize.activate.drawercheckout'
-});
+if (document.querySelector('.Cart-footer button.is-disabled')) {
+  console.debug('<experiment> activate drawercheckout');
+  dataLayer.push({
+    event: 'optimize.activate.drawercheckout'
+  });
+}
