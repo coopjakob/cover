485ff36c18ecc292ce2f7c1d9b75955a6357a9f0
Jakob Nanneson <jakob.nanneson@coop.se>
2021-04-19 15:48:44 +0200

    Changed variation in abtest=true

--- sortingorder/abtest-true.js
+++ sortingorder/abtest-true.js
@@ -23,7 +23,7 @@ if (queries.get('variant') == '2') {
   }
 
   function run() {
-    if (!document.querySelector('.experiment.t41')) {
+    if (!document.querySelector('.experiment.t49')) {
       var wrapper;
 
       if (window.location.pathname.startsWith('/handla/varor/')) {
@@ -33,7 +33,7 @@ if (queries.get('variant') == '2') {
       }
 
       var label = document.createElement('p');
-      label.classList.add('experiment', 't41', 'u-pullLeft', 'u-marginAxsm');
+      label.classList.add('experiment', 't49', 'u-pullLeft', 'u-marginAxsm');
       label.innerText = 'Sortera:';
       wrapper.prepend(label);
     }
@@ -62,21 +62,31 @@ if (queries.get('variant') == '2') {
   }
 
   function run() {
-    var element;
+    if (!document.querySelector('.experiment.t49')) {
+      var element;
 
-    if (window.location.pathname.startsWith('/handla/varor/')) {
-      element = document.querySelector('.js-accordionFilter > .u-flex > .u-sm-flex .Dropdown-header');
-    } else {
-      element = document.querySelector('.Dropdown.Dropdown--stripped.Dropdown--limitHeight .Dropdown-header');
+      if (window.location.pathname.startsWith('/handla/varor/')) {
+        element = document.querySelector('.js-accordionFilter > .u-flex > .u-sm-flex .Dropdown-header');
+      } else {
+        element = document.querySelector('.Dropdown.Dropdown--stripped.Dropdown--limitHeight .Dropdown-header');
+      }
+
+      element.classList.add('experiment', 't49');
+      var experimentStyle = document.createElement('style');
+      experimentStyle.innerHTML = "\n        .Dropdown .Dropdown-header {\n          border: 1px solid #777777!important;\n        }\n      ";
+      document.body.appendChild(experimentStyle);
+      var wrapper;
+
+      if (window.location.pathname.startsWith('/handla/varor/')) {
+        wrapper = document.querySelector('.js-accordionFilter > .u-flex > .u-sm-flex .Dropdown');
+      } else {
+        wrapper = document.querySelector('.Dropdown.Dropdown--stripped.Dropdown--limitHeight');
+      }
+
+      var label = document.createElement('p');
+      label.classList.add('experiment', 't49', 'u-pullLeft', 'u-marginAxsm');
+      label.innerText = 'Sortera:';
+      wrapper.prepend(label);
     }
-
-    element.classList.add('experiment', 't41');
-    element.style.backgroundRepeat = 'no-repeat';
-    element.style.backgroundPosition = '16px center';
-    element.style.backgroundImage = 'url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTciIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAxNyAxNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTEzIDMuNTcxMjlWMTIuOTk5OSIgc3Ryb2tlPSIjMzMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPHBhdGggZD0iTTE2LjQyODQgOS41NzIyN0wxMi45OTk5IDEzLjAwMDhMOS41NzEyOSA5LjU3MjI3IiBzdHJva2U9IiMzMzMzMzMiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPgo8cGF0aCBkPSJNNC40Mjg3MSAxMC40Mjg2VjEiIHN0cm9rZT0iIzMzMzMzMyIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+CjxwYXRoIGQ9Ik0xIDQuNDI4NTdMNC40Mjg1NyAxTDcuODU3MTQgNC40Mjg1NyIgc3Ryb2tlPSIjMzMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg==)';
-    element.style.paddingLeft = '41px';
-    var experimentStyle = document.createElement('style');
-    experimentStyle.innerHTML = "\n      .Dropdown .Dropdown-header {\n        border: 1px solid #777777!important;\n      }\n    ";
-    document.body.appendChild(experimentStyle);
   }
 }
--- sortingorder/abtest-true.ts
+++ sortingorder/abtest-true.ts
@@ -23,7 +23,7 @@ if (queries.get('variant') == '2') {
   }
 
   function run() {
-    if (!document.querySelector('.experiment.t41')) {
+    if (!document.querySelector('.experiment.t49')) {
       let wrapper;
 
       if (window.location.pathname.startsWith('/handla/varor/')) {
@@ -37,7 +37,7 @@ if (queries.get('variant') == '2') {
       }
 
       let label = document.createElement('p');
-      label.classList.add('experiment', 't41', 'u-pullLeft', 'u-marginAxsm');
+      label.classList.add('experiment', 't49', 'u-pullLeft', 'u-marginAxsm');
       label.innerText = 'Sortera:';
 
       wrapper.prepend(label);
@@ -66,32 +66,46 @@ if (queries.get('variant') == '2') {
   }
 
   function run() {
-    let element;
+    if (!document.querySelector('.experiment.t49')) {
+      let element;
 
-    if (window.location.pathname.startsWith('/handla/varor/')) {
-      element = document.querySelector(
-        '.js-accordionFilter > .u-flex > .u-sm-flex .Dropdown-header'
-      );
-    } else {
-      element = document.querySelector(
-        '.Dropdown.Dropdown--stripped.Dropdown--limitHeight .Dropdown-header'
-      );
-    }
-
-    element.classList.add('experiment', 't41');
-
-    element.style.backgroundRepeat = 'no-repeat';
-    element.style.backgroundPosition = '16px center';
-    element.style.backgroundImage =
-      'url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTciIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAxNyAxNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTEzIDMuNTcxMjlWMTIuOTk5OSIgc3Ryb2tlPSIjMzMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPHBhdGggZD0iTTE2LjQyODQgOS41NzIyN0wxMi45OTk5IDEzLjAwMDhMOS41NzEyOSA5LjU3MjI3IiBzdHJva2U9IiMzMzMzMzMiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPgo8cGF0aCBkPSJNNC40Mjg3MSAxMC40Mjg2VjEiIHN0cm9rZT0iIzMzMzMzMyIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+CjxwYXRoIGQ9Ik0xIDQuNDI4NTdMNC40Mjg1NyAxTDcuODU3MTQgNC40Mjg1NyIgc3Ryb2tlPSIjMzMzMzMzIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg==)';
-    element.style.paddingLeft = '41px';
-
-    let experimentStyle = document.createElement('style');
-    experimentStyle.innerHTML = `
-      .Dropdown .Dropdown-header {
-        border: 1px solid #777777!important;
+      if (window.location.pathname.startsWith('/handla/varor/')) {
+        element = document.querySelector(
+          '.js-accordionFilter > .u-flex > .u-sm-flex .Dropdown-header'
+        );
+      } else {
+        element = document.querySelector(
+          '.Dropdown.Dropdown--stripped.Dropdown--limitHeight .Dropdown-header'
+        );
       }
-    `;
-    document.body.appendChild(experimentStyle);
+
+      element.classList.add('experiment', 't49');
+
+      let experimentStyle = document.createElement('style');
+      experimentStyle.innerHTML = `
+        .Dropdown .Dropdown-header {
+          border: 1px solid #777777!important;
+        }
+      `;
+      document.body.appendChild(experimentStyle);
+
+      let wrapper;
+
+      if (window.location.pathname.startsWith('/handla/varor/')) {
+        wrapper = document.querySelector(
+          '.js-accordionFilter > .u-flex > .u-sm-flex .Dropdown'
+        );
+      } else {
+        wrapper = document.querySelector(
+          '.Dropdown.Dropdown--stripped.Dropdown--limitHeight'
+        );
+      }
+
+      let label = document.createElement('p');
+      label.classList.add('experiment', 't49', 'u-pullLeft', 'u-marginAxsm');
+      label.innerText = 'Sortera:';
+
+      wrapper.prepend(label);
+    }
   }
 }
