dcb9539e0a31f23ad692664f122a7db257c0f3c9
Jakob Nanneson <jakob.nanneson@coop.se>
2021-04-19 14:15:40 +0200

    Adding new design (position, text and style)

--- topnotice/variant1.js
+++ topnotice/variant1.js
@@ -1,21 +1,13 @@
-var wrapper = document.querySelector('.Main-container .Section .Grid');
+var wrapper = document.querySelector('.js-page');
 var element = document.createElement('div');
 element.classList.add('experiment', 't50', 'Grid-cell', 'u-sizeFull');
-
-if (element.clientWidth > 310) {
-  element.innerHTML = 'Spara tid – Handla hemma<br>Hämta i butik';
-} else {
-  element.innerText = 'Spara tid – Handla hemma – Hämta i butik';
-}
-
-element.style.padding = '5px 10px';
-element.style.border = '1px solid #005537';
-element.style.borderTop = 'unset';
-element.style.backgroundColor = 'white';
-element.style.borderRadius = '0 0 6px 6px';
-element.style.fontSize = '0.9em';
+element.innerText = 'Spara tid – Handla hemma – Flexibel leverans';
+element.style.padding = '9px';
+element.style.backgroundColor = '#00AA46';
+element.style.fontSize = '0.75em';
 element.style.textAlign = 'center';
+element.style.color = 'white';
+element.style.marginBottom = '1.25em';
 wrapper.prepend(element);
-var style = document.createElement('style');
-style.innerHTML = "\n  @media only screen and (max-width: 1025px) {\n    .js-childLayoutContainer.u-marginTmd {\n      margin-top: 10px !important;\n    }\n  }\n";
-wrapper.prepend(style);
+var container = document.querySelector('.js-childLayoutContainer');
+container.classList.remove('u-marginTmd');
--- topnotice/variant1.ts
+++ topnotice/variant1.ts
@@ -1,29 +1,18 @@
-let wrapper = document.querySelector('.Main-container .Section .Grid');
+let wrapper = document.querySelector('.js-page');
 
 let element = document.createElement('div');
 element.classList.add('experiment', 't50', 'Grid-cell', 'u-sizeFull');
 
-if (element.clientWidth > 310) {
-  element.innerHTML = 'Spara tid – Handla hemma<br>Hämta i butik';
-} else {
-  element.innerText = 'Spara tid – Handla hemma – Hämta i butik';
-}
+element.innerText = 'Spara tid – Handla hemma – Flexibel leverans';
 
-element.style.padding = '5px 10px';
-element.style.border = '1px solid #005537';
-element.style.borderTop = 'unset';
-element.style.backgroundColor = 'white';
-element.style.borderRadius = '0 0 6px 6px';
-element.style.fontSize = '0.9em';
+element.style.padding = '9px';
+element.style.backgroundColor = '#00AA46';
+element.style.fontSize = '0.75em';
 element.style.textAlign = 'center';
+element.style.color = 'white';
+element.style.marginBottom = '1.25em';
+
 wrapper.prepend(element);
 
-let style = document.createElement('style');
-style.innerHTML = `
-  @media only screen and (max-width: 1025px) {
-    .js-childLayoutContainer.u-marginTmd {
-      margin-top: 10px !important;
-    }
-  }
-`;
-wrapper.prepend(style);
+let container = document.querySelector('.js-childLayoutContainer');
+container.classList.remove('u-marginTmd');
