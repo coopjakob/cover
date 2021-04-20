ed5ba59340dc6a000530cdc50687638ed60eef97
Jakob Nanneson <jakob.nanneson@coop.se>
2021-04-20 17:40:48 +0200

    Adding orginal interaction event to variant

--- drawercheckout/variant1.js
+++ drawercheckout/variant1.js
@@ -1,6 +1,12 @@
 var element = document.querySelector('.Cart-footer button');
 element.textContent = 'Gå till kassan';
 element.addEventListener('click', function (event) {
+  dataLayer.push({
+    event: 'interaction',
+    eventCategory: 'Ecommerce',
+    eventAction: 'Slider - Varukorg - Överblick',
+    eventLabel: 'Gå till kassan'
+  });
   location.href = '/handla/betala/';
 });
 element.classList.remove('is-disabled');
--- drawercheckout/variant1.ts
+++ drawercheckout/variant1.ts
@@ -2,6 +2,12 @@ let element = document.querySelector('.Cart-footer button');
 
 element.textContent = 'Gå till kassan';
 element.addEventListener('click', (event) => {
+  dataLayer.push({
+    event: 'interaction',
+    eventCategory: 'Ecommerce',
+    eventAction: 'Slider - Varukorg - Överblick',
+    eventLabel: 'Gå till kassan',
+  });
   location.href = '/handla/betala/';
 });
 element.classList.remove('is-disabled');
