62aeac4ab4b776ce1dab4aa634aa999a14fb7a8c
Jakob Nanneson <jakob.nanneson@coop.se>
2021-04-22 13:55:34 +0200

    Adding style without repaint and include svg

--- searchbutton/variant1.ts
+++ searchbutton/variant1.ts
@@ -1,38 +1,38 @@
+let search = document.querySelector('.Search.Search--green');
+
+let input = search.querySelector('.Search-input');
+input.style.paddingLeft = '1rem';
+
+let icon = search.querySelector('.Search.Search--online .Search-icon');
+icon.style.top = '-1px';
+icon.style.right = '-1px';
+icon.style.left = 'unset';
+icon.style.width = '40px';
+icon.style.height = '44px';
+icon.style.padding = '0';
+icon.style.borderRight = '4px solid #00aa46';
+icon.style.backgroundColor = '#00aa46';
+icon.style.borderRadius = '0 22px 22px 0';
+
+// Include svg to be able to set color of path
+let svg = icon.querySelector('svg');
+svg.setAttribute('viewBox', '0 0 20 20');
+svg.innerHTML =
+  '<path xmlns="http://www.w3.org/2000/svg" d="M19 19l-4.64-4.64m-5.534 2.292A7.826 7.826 0 108.826 1a7.826 7.826 0 000 15.652z" stroke-linecap="round" stroke-linejoin="round" stroke="white"/>';
+
+// Add style for elements that isn't visible yet
 let style = document.createElement('style');
 style.innerHTML = `
-  .Search.Search--online .Search-icon {
-    top: -1px;
-    right: -1px;
-    left: unset;
-    width: 40px;
-    height: 44px;
-    padding: 0;
-    border-right: 4px solid #00aa46;
-    background-color: #00aa46;
-    border-radius: 0 22px 22px 0;
-  }
-
   .Search.Search--radiusDropdown.is-active .Search-icon {
-    border-radius: 0 22px 0 0;
+    border-radius: 0 22px 0 0 !important;
   }
-
   .Search-clear {
     right: 44px !important;
   }
-
-  .Search-input {
-    padding-left: 1rem;
-  }
-
-  .Search.Search--green .Search-content .Search-icon svg {
-    stroke: white !important;
-  }
-`;
+  `;
 document.body.appendChild(style);
 
-let element = document.querySelector('.Search.Search--online .Search-icon');
-
-element.addEventListener('click', () => {
+icon.addEventListener('click', () => {
   console.debug('<experiment> click searchbutton');
   dataLayer.push({
     event: 'interaction',
@@ -45,5 +45,5 @@ element.addEventListener('click', () => {
     cancelable: true,
     keyCode: 13,
   });
-  document.querySelector('.Search-input').dispatchEvent(enterkey);
+  input.dispatchEvent(enterkey);
 });
