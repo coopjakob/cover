f92bcbb9650253d71d05e1b883f6e2b8e5f8197d
Jakob Nanneson <jakob.nanneson@coop.se>
2021-05-06 14:52:38 +0200

    Removed duplicated code and minor fixes

--- buybutton/variant1.js
+++ buybutton/variant1.js
@@ -1,26 +1,18 @@
 function addButtonLabels() {
-  document.querySelectorAll('.ItemTeaser-button .Button').forEach(function (element) {
-    if (element.parentElement.querySelector('input').value == 0) {
+  document.querySelectorAll('.ItemTeaser-button > .Button, .ProductSearch-itemCta > .Button').forEach(function (element) {
+    var isItemTeaser = element.parentElement.classList.contains('.ItemTeaser-button');
+
+    if (element.parentElement.querySelector('input').value === 0) {
       element.classList.remove('u-hidden');
       element.parentElement.querySelector('.AddToCart').classList.add('u-hidden');
+      element.style.minWidth = '120px';
       element.style.textOverflow = 'unset';
       element.style.paddingLeft = 0;
       element.style.paddingRight = 0;
       element.textContent = 'Lägg till';
-    } else {
+    } else if (isItemTeaser) {
       element.classList.add('u-hidden');
       element.parentElement.querySelector('.AddToCart').classList.remove('u-hidden');
-    }
-  });
-  document.querySelectorAll('.ProductSearch-itemCta .Button').forEach(function (element) {
-    if (element.parentElement.querySelector('input').value == 0) {
-      element.classList.remove('u-hidden');
-      element.parentElement.querySelector('.AddToCart').classList.add('u-hidden');
-      element.style.textOverflow = 'unset';
-      element.style.paddingLeft = 0;
-      element.style.paddingRight = 0;
-      element.style.width = '120px';
-      element.textContent = 'Lägg till';
     }
   });
 }
--- buybutton/variant1.ts
+++ buybutton/variant1.ts
@@ -1,40 +1,30 @@
 function addButtonLabels() {
-  // startsidan, sökresultat, kategorier
-  document.querySelectorAll('.ItemTeaser-button .Button').forEach((element) => {
-    if (element.parentElement.querySelector('input').value == 0) {
-      element.classList.remove('u-hidden');
-      element.parentElement
-        .querySelector('.AddToCart')
-        .classList.add('u-hidden');
-
-      element.style.textOverflow = 'unset';
-      element.style.paddingLeft = 0;
-      element.style.paddingRight = 0;
-      element.textContent = 'Lägg till';
-    } else {
-      element.classList.add('u-hidden');
-      element.parentElement
-        .querySelector('.AddToCart')
-        .classList.remove('u-hidden');
-    }
-  });
-
-  // dropdown på handla
   document
-    .querySelectorAll('.ProductSearch-itemCta .Button')
+    .querySelectorAll(
+      '.ItemTeaser-button > .Button, .ProductSearch-itemCta > .Button'
+    )
     .forEach((element) => {
-      if (element.parentElement.querySelector('input').value == 0) {
-        element.classList.remove('u-hidden');
+      let isItemTeaser = element.parentElement.classList.contains(
+        '.ItemTeaser-button'
+      );
 
+      if (element.parentElement.querySelector('input').value === 0) {
+        element.classList.remove('u-hidden');
         element.parentElement
           .querySelector('.AddToCart')
           .classList.add('u-hidden');
 
+        element.style.minWidth = '120px';
         element.style.textOverflow = 'unset';
         element.style.paddingLeft = 0;
         element.style.paddingRight = 0;
-        element.style.width = '120px';
+
         element.textContent = 'Lägg till';
+      } else if (isItemTeaser) {
+        element.classList.add('u-hidden');
+        element.parentElement
+          .querySelector('.AddToCart')
+          .classList.remove('u-hidden');
       }
     });
 }
@@ -49,6 +39,7 @@ window.addEventListener('ga:modifyCart', () => {
   clearTimeout(setLabelAfterTimeout);
   setLabelAfterTimeout = setTimeout(addButtonLabels, 1000);
 });
+
 window.addEventListener('resize', () => {
   clearTimeout(setLabelAfterTimeout);
   setLabelAfterTimeout = setTimeout(addButtonLabels, 200);
