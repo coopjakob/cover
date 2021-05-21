539d8c27f25ab2121343618a79f42b4cbc118c6d
Jakob Nanneson <jakob.nanneson@coop.se>
2021-05-21 14:30:21 +0200

    Functions is working

--- storefocus/variant1.js
+++ storefocus/variant1.js
@@ -1,4 +1,4 @@
-var _document$querySelect;
+var _document$querySelect, _document$querySelect2;
 
 function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
 
@@ -7,38 +7,92 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
 var itemsInCart = (_document$querySelect = document.querySelector('.CartButton .Badge')) === null || _document$querySelect === void 0 ? void 0 : _document$querySelect.textContent;
+var isStoreSet = !!((_document$querySelect2 = document.querySelector('.TimeslotPreview-info')) === null || _document$querySelect2 === void 0 ? void 0 : _document$querySelect2.textContent);
 
-if (itemsInCart === '0') {
+if (!isStoreSet) {}
+
+var portalObserver = new MutationObserver(function (mutations) {
+  var _iterator = _createForOfIteratorHelper(mutations),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var addedNodes = _step.value.addedNodes;
+
+      if (containClassInNodes(addedNodes, 'FlyIn-header')) {
+        portalObserver.disconnect();
+        run();
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+});
+
+if (isStoreSet) {
   window.addEventListener('ga:modifyCart', function () {
     run();
-  }, {
-    once: true
+  });
+} else {
+  portalObserver.observe(document.getElementById('portal'), {
+    childList: true,
+    subtree: true
   });
 }
 
 var modalContainer;
 
 function run() {
-  document.querySelector('.CartButton').click();
+  if (isStoreSet) {
+    document.querySelector('.CartButton').click();
+  }
+
+  modalContainer = document.querySelector('#portal .Modal-container');
+
+  if (isStoreSet) {
+    var modalContainerObserver = new MutationObserver(function (mutations) {
+      var _iterator2 = _createForOfIteratorHelper(mutations),
+          _step2;
+
+      try {
+        for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+          var addedNodes = _step2.value.addedNodes;
+
+          if (containClassInNodes(addedNodes, 'Cart-header')) {
+            modalContainerObserver.disconnect();
+            getVariables();
+          }
+        }
+      } catch (err) {
+        _iterator2.e(err);
+      } finally {
+        _iterator2.f();
+      }
+    });
+    modalContainerObserver.observe(modalContainer, {
+      childList: true,
+      subtree: true
+    });
+  } else {
+    createBox();
+  }
+
   var modal = document.querySelector('#portal .Modal.Modal--right');
   modal.classList.remove('Modal--right');
   modal.classList.add('Modal--center');
-  modalContainer = document.querySelector('#portal .Modal-container');
-  modalContainerObserver.observe(modalContainer, {
-    childList: true,
-    subtree: true
-  });
 }
 
 function containClassInNodes(nodes, containClass) {
   var foundNode = false;
 
-  var _iterator = _createForOfIteratorHelper(nodes),
-      _step;
+  var _iterator3 = _createForOfIteratorHelper(nodes),
+      _step3;
 
   try {
-    for (_iterator.s(); !(_step = _iterator.n()).done;) {
-      var node = _step.value;
+    for (_iterator3.s(); !(_step3 = _iterator3.n()).done;) {
+      var node = _step3.value;
 
       if (node.childNodes) {
         foundNode = containClassInNodes(node.childNodes, containClass);
@@ -52,43 +106,24 @@ function containClassInNodes(nodes, containClass) {
       }
     }
   } catch (err) {
-    _iterator.e(err);
+    _iterator3.e(err);
   } finally {
-    _iterator.f();
+    _iterator3.f();
   }
 
   return foundNode;
 }
 
-var modalContainerObserver = new MutationObserver(function (mutations) {
-  var _iterator2 = _createForOfIteratorHelper(mutations),
-      _step2;
-
-  try {
-    for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
-      var addedNodes = _step2.value.addedNodes;
-
-      if (containClassInNodes(addedNodes, 'Cart-header')) {
-        modalContainerObserver.disconnect();
-        getVariables();
-      }
-    }
-  } catch (err) {
-    _iterator2.e(err);
-  } finally {
-    _iterator2.f();
-  }
-});
 var deliverymethod;
 var postalcode;
 var storename;
 
 function getVariables() {
-  var _document$querySelect2, _document$querySelect3, _document$querySelect4;
+  var _document$querySelect3, _document$querySelect4, _document$querySelect5;
 
-  deliverymethod = (_document$querySelect2 = document.querySelector('[data-test=cncheader-changedeliverymethod]')) === null || _document$querySelect2 === void 0 ? void 0 : _document$querySelect2.textContent;
-  postalcode = (_document$querySelect3 = document.querySelector('[data-test=changePostalCode]')) === null || _document$querySelect3 === void 0 ? void 0 : _document$querySelect3.textContent;
-  storename = (_document$querySelect4 = document.querySelector('[data-test=pickupStoreLink]')) === null || _document$querySelect4 === void 0 ? void 0 : _document$querySelect4.textContent;
+  deliverymethod = (_document$querySelect3 = document.querySelector('[data-test=cncheader-changedeliverymethod]')) === null || _document$querySelect3 === void 0 ? void 0 : _document$querySelect3.textContent;
+  postalcode = (_document$querySelect4 = document.querySelector('[data-test=changePostalCode]')) === null || _document$querySelect4 === void 0 ? void 0 : _document$querySelect4.textContent;
+  storename = (_document$querySelect5 = document.querySelector('[data-test=pickupStoreLink]')) === null || _document$querySelect5 === void 0 ? void 0 : _document$querySelect5.textContent;
   createBox();
 }
 
@@ -96,24 +131,30 @@ function createBox() {
   var questionbox = document.createElement('div');
   questionbox.classList.add('u-flex', 'u-flexDirectionColumn', 'u-bgWhite', 'u-sizeFull', 'u-sm-size540');
   questionbox.style.position = 'absolute';
-  questionbox.innerText = "Ditt val fr\xE5n tidigare k\xF6p \xE4r ".concat(deliverymethod);
-  var okbutton = document.createElement('button');
-  okbutton.innerText = 'Jag vill ändra';
-  okbutton.addEventListener('click', function () {
-    document.querySelector('[data-test=cncheader-chagedeliverymethodbutton]').click();
-    questionbox.remove();
-    containerDiv.classList.add('u-flex');
-    containerDiv.classList.remove('u-hidden');
-  });
-  questionbox.append(okbutton);
-  var cancelbutton = document.createElement('button');
-  cancelbutton.innerText = 'Fortsätt handla';
-  cancelbutton.addEventListener('click', function () {
-    document.querySelector('.FlyIn-close').click();
-    questionbox.remove();
-  });
-  questionbox.append(cancelbutton);
   var containerDiv = modalContainer.querySelector('div');
+
+  if (deliverymethod) {
+    questionbox.innerText = "Ditt val fr\xE5n tidigare k\xF6p \xE4r ".concat(deliverymethod);
+    var okbutton = document.createElement('button');
+    okbutton.innerText = 'Jag vill ändra';
+    okbutton.addEventListener('click', function () {
+      document.querySelector('[data-test=cncheader-chagedeliverymethodbutton]').click();
+      questionbox.remove();
+      containerDiv.classList.add('u-flex');
+      containerDiv.classList.remove('u-hidden');
+    });
+    questionbox.append(okbutton);
+    var cancelbutton = document.createElement('button');
+    cancelbutton.innerText = 'Fortsätt handla';
+    cancelbutton.addEventListener('click', function () {
+      document.querySelector('.FlyIn-close').click();
+      questionbox.remove();
+    });
+    questionbox.append(cancelbutton);
+  } else {
+    questionbox.innerText = 'Välkommen!';
+  }
+
   containerDiv.classList.remove('u-flex');
   containerDiv.classList.add('u-hidden');
   modalContainer.prepend(questionbox);
--- storefocus/variant1.ts
+++ storefocus/variant1.ts
@@ -1,39 +1,104 @@
 let itemsInCart = document.querySelector('.CartButton .Badge')?.textContent;
 
-if (itemsInCart === '0') {
-  console.debug('0 in cart');
+let isStoreSet = !!document.querySelector('.TimeslotPreview-info')?.textContent;
 
-  window.addEventListener(
-    'ga:modifyCart',
-    () => {
-      console.debug('ga:modifyCart');
-      // dataLayer.push({
-      //   event: 'optimize.activate.storefocus'
-      // });
-      run();
-    },
-    {
-      once: true,
-    }
-  );
+if (!isStoreSet) {
 }
+
+// if (itemsInCart === '0') {
+//   console.debug('0 in cart');
+
+//   window.addEventListener(
+//     'ga:modifyCart',
+//     () => {
+//       console.debug('ga:modifyCart');
+//       // dataLayer.push({
+//       //   event: 'optimize.activate.storefocus'
+//       // });
+//       run();
+//     },
+//     {
+//       once: true,
+//     }
+//   );
+// }
+
+let portalObserver = new MutationObserver((mutations) => {
+  console.debug('<experiment> portal change detected');
+  for (const { addedNodes } of mutations) {
+    console.debug('<experiment> added node', addedNodes);
+    if (containClassInNodes(addedNodes, 'FlyIn-header')) {
+      console.debug('<experiment> FlyIn-header exist');
+      portalObserver.disconnect();
+      // addWelcomeContainer();
+      run();
+    }
+  }
+});
+
+if (isStoreSet) {
+  window.addEventListener('ga:modifyCart', () => {
+    console.debug('ga:modifyCart');
+    run();
+  });
+} else {
+  portalObserver.observe(document.getElementById('portal'), {
+    childList: true,
+    subtree: true,
+  });
+}
+
+// window.addEventListener('ga:emptyCart', () => {
+//   console.debug('ga:emptyCart');
+//   run();
+// });
+
+// window.addEventListener('ga:deliveryInfo', () => {
+//   console.debug('ga:deliveryInfo');
+//   run();
+// });
+
+// window.addEventListener('ga:existingCartAddToCart', () => {
+//   console.debug('ga:existingCartAddToCart');
+//   run();
+// });
 
 let modalContainer;
 function run() {
-  document.querySelector('.CartButton').click();
+  if (isStoreSet) {
+    document.querySelector('.CartButton').click();
+  }
+
+  modalContainer = document.querySelector('#portal .Modal-container');
+
+  if (isStoreSet) {
+    let modalContainerObserver = new MutationObserver((mutations) => {
+      console.debug('<experiment> modal change detected');
+      for (const { addedNodes } of mutations) {
+        console.debug('<experiment> added node', addedNodes);
+
+        if (containClassInNodes(addedNodes, 'Cart-header')) {
+          console.debug('<experiment> Cart-header exist');
+          modalContainerObserver.disconnect();
+          getVariables();
+        }
+      }
+    });
+
+    console.debug('<experiment> observing Modal-container', modalContainer);
+    modalContainerObserver.observe(modalContainer, {
+      childList: true,
+      subtree: true,
+    });
+  } else {
+    createBox();
+  }
 
   let modal = document.querySelector('#portal .Modal.Modal--right');
   console.debug('Check for modal:', modal);
 
   modal.classList.remove('Modal--right');
   modal.classList.add('Modal--center');
-
-  modalContainer = document.querySelector('#portal .Modal-container');
-  console.debug('<experiment> observing Modal-container', modalContainer);
-  modalContainerObserver.observe(modalContainer, {
-    childList: true,
-    subtree: true,
-  });
 }
 
 // document.querySelector('.Modal-container').style.padding = '2em';
@@ -55,18 +120,6 @@ function containClassInNodes(nodes, containClass) {
   }
   return foundNode;
 }
-
-let modalContainerObserver = new MutationObserver((mutations) => {
-  console.debug('<experiment> change detected');
-  for (const { addedNodes } of mutations) {
-    console.debug('<experiment> added node', addedNodes);
-    if (containClassInNodes(addedNodes, 'Cart-header')) {
-      console.debug('<experiment> Cart-header exist');
-      modalContainerObserver.disconnect();
-      getVariables();
-    }
-  }
-});
 
 let deliverymethod;
 let postalcode;
@@ -100,32 +153,37 @@ function createBox() {
 
   questionbox.style.position = 'absolute';
 
-  questionbox.innerText = `Ditt val från tidigare köp är ${deliverymethod}`;
-
-  let okbutton = document.createElement('button');
-  okbutton.innerText = 'Jag vill ändra';
-  okbutton.addEventListener('click', () => {
-    document
-      .querySelector('[data-test=cncheader-chagedeliverymethodbutton]')
-      .click();
-    questionbox.remove();
-
-    // TODO: Is flex needed?
-    containerDiv.classList.add('u-flex');
-    containerDiv.classList.remove('u-hidden');
-  });
-  questionbox.append(okbutton);
-
-  let cancelbutton = document.createElement('button');
-  cancelbutton.innerText = 'Fortsätt handla';
-  cancelbutton.addEventListener('click', () => {
-    document.querySelector('.FlyIn-close').click();
-    questionbox.remove();
-  });
-  questionbox.append(cancelbutton);
-
   // let modalcontainer = document.querySelector('.Modal-container');
   let containerDiv = modalContainer.querySelector('div');
+
+  if (deliverymethod) {
+    questionbox.innerText = `Ditt val från tidigare köp är ${deliverymethod}`;
+
+    let okbutton = document.createElement('button');
+    okbutton.innerText = 'Jag vill ändra';
+    okbutton.addEventListener('click', () => {
+      document
+        .querySelector('[data-test=cncheader-chagedeliverymethodbutton]')
+        .click();
+      questionbox.remove();
+
+      // TODO: Is flex needed?
+      containerDiv.classList.add('u-flex');
+      containerDiv.classList.remove('u-hidden');
+    });
+    questionbox.append(okbutton);
+
+    let cancelbutton = document.createElement('button');
+    cancelbutton.innerText = 'Fortsätt handla';
+    cancelbutton.addEventListener('click', () => {
+      document.querySelector('.FlyIn-close').click();
+      questionbox.remove();
+    });
+    questionbox.append(cancelbutton);
+  } else {
+    questionbox.innerText = 'Välkommen!';
+  }
+
   containerDiv.classList.remove('u-flex');
   containerDiv.classList.add('u-hidden');
 
