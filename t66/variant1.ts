721df1e08f6b0f637c8c2bfa60ab19acad15d3aa
Jakob Nanneson <jakob.nanneson@coop.se>
2021-09-06 20:17:15 +0200

    Remove full notice bar on handla start page

--- t66/variant1.ts
+++ t66/variant1.ts
@@ -1,5 +1,11 @@
 document.querySelector('.T66')?.classList.add('u-hidden');
 
+if (window.location.pathname == '/handla/') {
+  document
+    .querySelector('[data-react-component="NoticeRibbonDefaultStore"]')
+    ?.classList.add('u-hidden');
+}
+
 document.addEventListener('cover.ready T66', (event) => {
   console.log(event);
   event.target.classList.add('u-hidden');
