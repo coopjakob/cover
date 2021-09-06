b50f1c5b791315c3d38d5bced5098d7b3a5c245a
Jakob Nanneson <jakob.nanneson@coop.se>
2021-09-06 15:26:01 +0200

    Debug using checks and warnings

--- t67/variant1.ts
+++ t67/variant1.ts
@@ -1,6 +1,14 @@
-document.querySelector('.T67')?.classList.add('u-hidden');
+let button = document.querySelector(
+  '[data-test="cncheader-chagedeliverymethodbutton"]'
+);
+
+if (button) {
+  button.classList.add('u-hidden');
+} else {
+  console.warn('<experiment> No button available');
+}
 
 document.addEventListener('cover.ready T67', (event) => {
-  console.log(event);
+  console.warn('<experiment> Using event listener', event);
   event.target.classList.add('u-hidden');
 });
