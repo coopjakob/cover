a1d3d4fd161cf892d16e163ba3c5cd6d4460c9b6
Jakob Nanneson <jakob.nanneson@coop.se>
2021-09-15 10:38:49 +0200

    Rename from js to ts to make file public

---
+++ t63/variant1.ts
@@ -0,0 +1,12 @@
+t63(document.querySelector('.Experiment.T63'));
+
+document.addEventListener('cover.ready T63', (element) => {
+  t63(element);
+});
+
+function t63(element) {
+  const firstBuy = document
+    .querySelector('[data-list="First Buy"]')
+    .closest('.Grid-cell.u-sizeFull');
+  element.parentNode.insertBefore(element, firstBuy.nextSibling);
+}
