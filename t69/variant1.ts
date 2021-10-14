1ff5db798650efa439b36547307b35a149eba034
Jakob Nanneson <jakob.nanneson@coop.se>
2021-10-14 10:03:54 +0200

    Replace existing link and click

---
+++ t69/variant1.ts
@@ -0,0 +1,11 @@
+t69();
+
+document.addEventListener('cover.ready T69', () => {
+  t69();
+});
+
+function t69() {
+  let a = document.querySelector('[data-id="81297"] a');
+  a.setAttribute('href', '/handla/aktuella-erbjudanden/v1/');
+  a.click();
+}
