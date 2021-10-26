2a0e9a1f745be6b5d4babcc81e7d3e4d4f9f7f02
Jakob Nanneson <jakob.nanneson@coop.se>
2021-10-26 18:44:59 +0200

    Max width of 220 px on product cards

--- t73/variant1.ts
+++ t73/variant1.ts
@@ -10,6 +10,7 @@ function t73() {
       .T73.Grid--product>.Grid-cell {
         flex-basis: 170px;
         flex-grow: 1;
+        max-width: 220px;
       }`;
 
   document.body.append(experimentStyle);
