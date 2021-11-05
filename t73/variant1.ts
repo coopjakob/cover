a04bdd52dd7ba8e9e923cb7717a26280f0261afb
Jakob Nanneson <jakob.nanneson@coop.se>
2021-11-05 10:42:23 +0100

    Only add style once per pageview

--- t73/variant1.ts
+++ t73/variant1.ts
@@ -1,17 +1,9 @@
-t73();
-
-document.addEventListener('cover.ready T73', (event) => {
-  t73();
-});
-
-function t73() {
-  const experimentStyle = document.createElement('style');
-  experimentStyle.innerHTML = `
+const experimentStyle = document.createElement('style');
+experimentStyle.innerHTML = `
       .T73.Grid--product>.Grid-cell {
         flex-basis: 170px;
         flex-grow: 1;
         max-width: 220px;
       }`;
 
-  document.body.append(experimentStyle);
-}
+document.body.append(experimentStyle);
