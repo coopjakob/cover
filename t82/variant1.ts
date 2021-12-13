f849e2de344c164cd12a504e9e0b029c9ae1c633
Jakob Nanneson <jakob.nanneson@coop.se>
2021-12-13 09:23:33 +0100

    Center on phone and period at end of sentence

--- t82/variant1.ts
+++ t82/variant1.ts
@@ -11,19 +11,21 @@ function t82(element = document.querySelector('.Experiment.T82')) {
     const button = element.querySelector('.Button');
 
     element.style.minHeight = 'unset';
+    element.style.textAlign = 'center';
 
     title.innerText = 'Medlemmar tjänar mer!';
+    title.classList.add('u-sizeFull', 'u-sm-textLeft');
 
     body.innerText =
-      'Samla poäng och handla till medlemspriser även på Coop.se – anslut ditt medlemskap';
-    body.classList.add('u-marginBxsm');
+      'Samla poäng och handla till medlemspriser även på Coop.se – anslut ditt medlemskap.';
+    body.classList.add('u-marginBxsm', 'u-sm-textLeft');
 
     const logo = document.createElement('span');
     logo.innerHTML = `
       <svg class="Icon Icon--bankid u-alignTextTop u-marginLxsm" role="img">
-      <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/assets/build/sprite.svg?v=210223.1040#bankid">
-      <title>BankID</title>
-      </use>
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/assets/build/sprite.svg?v=210223.1040#bankid">
+          <title>BankID</title>
+        </use>
       </svg>
     `;
 
