a6ae9f889e3dd63d564e00129f50a5a5f61437d7
Jakob Nanneson <jakob.nanneson@coop.se>
2021-12-20 13:12:29 +0100

    Spread querySelectorAll NodeList to an array

--- events.ts
+++ events.ts
@@ -60,9 +60,7 @@ const cover: coverType = {
       }
 
       if (options.querySelectorAll) {
-        elements = elements.concat(
-          Array.from(wrapper.querySelectorAll(selector))
-        );
+        elements = [...wrapper.querySelectorAll(selector)];
       } else {
         const selectorElement = wrapper.querySelector(selector);
         if (selectorElement) {
