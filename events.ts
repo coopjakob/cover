9ab7d444a72b6fdecae74131bbaedc25c9158b1a
Jakob Nanneson <jakob.nanneson@coop.se>
2021-12-21 19:13:41 +0100

    Add window.ga send event with beacon

--- events.ts
+++ events.ts
@@ -216,6 +216,12 @@ const cover: CoverType = {
                     DY.API('event', {
                       name: 'T82-click',
                     });
+                    window.ga('send', {
+                      hitType: 'event',
+                      eventCategory: 'Experiment',
+                      eventAction: 'T82-ga-click',
+                      transport: 'beacon',
+                    });
                     location.href = event.currentTarget.getAttribute('href');
                   });
               }
