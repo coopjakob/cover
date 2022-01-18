58c9b9e53768fd55c38b4ec3435119da773fe32f
Jakob Nanneson <jakob.nanneson@coop.se>
2022-01-18 13:40:12 +0100

    Remove support for DY API and features

--- events.ts
+++ events.ts
@@ -1,13 +1,10 @@
 declare const DY: any;
 declare const DYO: any;
 declare const __cmp: any;
-declare const dataLayer: any;
 
 interface CoverType {
   checkDynamicYieldABtestConsent: () => boolean;
   isInternetExplorer: boolean;
-  abtestFlag: () => string | null;
-  getCookieValue: (string) => string;
   waitFor: (
     selector: string,
     callback: (element: HTMLElement) => void,
@@ -20,10 +17,6 @@ interface CoverType {
   ) => void;
   isCategoryPage: () => boolean;
   isProductPage: (path?: string) => boolean;
-  choose: {
-    experiment: (id: string) => object;
-    promises: object;
-  };
   ready: (element: Element, id: string) => void;
   readyHistory: Array<string>;
   addIdentifierClasses: (element: Element, id: string) => void;
@@ -38,17 +31,6 @@ const cover: CoverType = {
   },
   // @ts-ignore
   isInternetExplorer: !!document.documentMode,
-  abtestFlag: () => {
-    //TODO: Change to constant if possible
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('abtest');
-  },
-  getCookieValue: (name) => {
-    return (
-      document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() ||
-      ''
-    );
-  },
   waitFor: (selector, callback, options = {}) => {
     let wrapperElement = document.body;
     let observer: MutationObserver;
@@ -137,80 +119,6 @@ const cover: CoverType = {
       return true;
     }
   },
-  choose: {
-    async experiment(experimentId) {
-      if (cover.abtestFlag()) {
-        console.log('abtest=' + cover.abtestFlag());
-        console.log(`Experiment ${experimentId} = TRUE`);
-        this.promises[experimentId] = true;
-        return true;
-      }
-
-      if (typeof this.promises[experimentId] !== 'undefined') {
-        return this.promises[experimentId];
-        // TODO: Check if we can remove else here
-      } else {
-        const response = await fetch(
-          'https://direct.dy-api.eu/v2/serve/user/choose',
-          {
-            method: 'POST',
-            headers: {
-              'Content-type': 'application/json',
-              'dy-api-key': '',
-            },
-            body: JSON.stringify({
-              user: {
-                dyid: cover.getCookieValue('_dyid'),
-                dyid_server: cover.getCookieValue('_dyid_server'),
-                active_consent_accepted: cover.checkDynamicYieldABtestConsent(),
-              },
-              session: {
-                dy: cover.getCookieValue('_dyjsession'),
-              },
-              selector: {
-                names: [experimentId],
-              },
-              context: {
-                page: {
-                  type: 'OTHER',
-                  data: [],
-                  location: window.location.href,
-                },
-              },
-              options: {
-                isImplicitPageview: false, //default in web mode
-              },
-            }),
-          }
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            const payload = data.choices[0].variations[0].payload.data;
-
-            if (!!payload) {
-              dataLayer.push({
-                event: 'DY event',
-                eventCategory: 'DY Smart Action',
-                eventAction: experimentId,
-                eventLabel: 'AB (Variation 1)',
-              });
-              return true;
-            } else {
-              dataLayer.push({
-                event: 'DY event',
-                eventCategory: 'DY Smart Action',
-                eventAction: experimentId,
-                eventLabel: 'AB (Control Group: No Action)',
-              });
-              return false;
-            }
-          });
-
-        return response;
-      }
-    },
-    promises: {},
-  },
   ready: (element, id) => {
     element.dispatchEvent(new Event(`cover.ready ${id}`, { bubbles: true }));
 
@@ -295,8 +203,6 @@ const cover: CoverType = {
       }
     );
 
-    // cover.choose.promises['AA'] = cover.choose.experiment('AA');
-
     pageview();
     cover.waitFor('.js-page', () => {
       pageview();
@@ -359,25 +265,6 @@ const cover: CoverType = {
                     cover.addIdentifierClasses(element, 'T86');
                     cover.ready(element, 'T86');
                   });
-
-                  // const firstList: HTMLStyleElement =
-                  //   element.querySelector('.SidebarNav-list');
-
-                  // firstList.style.opacity = '0';
-
-                  // const timeout = setTimeout(() => {
-                  //   firstList.style.opacity = 'unset';
-                  // }, 1000);
-
-                  // cover.choose.promises['T86'] = cover.choose.experiment('T86');
-
-                  // if (await cover.choose.promises['T86']) {
-                  //   selectedItems.forEach((element) => {
-                  //     element.remove();
-                  //   });
-                  // }
-                  // firstList.style.opacity = 'unset';
-                  // clearTimeout(timeout);
                 }
               });
             });
@@ -388,144 +275,13 @@ const cover: CoverType = {
           }
         );
       }
-
-      // if (window.location.pathname === '/handla/') {
-      //   cover.waitFor(
-      //     '[data-react-component="DynamicYieldRecommendationsBlock"]',
-      //     async (element) => {
-      //       const props = JSON.parse(element.dataset.reactProps);
-      //       const id = props.recommendationId;
-
-      //       if (
-      //         id === 'P04.favourite-products.handla-startpage' ||
-      //         id === 'P03.popular-products.handla-startpage' ||
-      //         id === 'Home_page.horizontal_recs1_b2b' ||
-      //         id === 'home_page.horizontal_recs4_b2b'
-      //       ) {
-      //         element.style.opacity = '0';
-
-      //         const timeout = setTimeout(() => {
-      //           element.style.opacity = 'unset';
-      //         }, 1000);
-
-      //         cover.choose.promises['T84'] = cover.choose.experiment('T84');
-
-      //         if (await cover.choose.promises['T84']) {
-      //           element.remove();
-      //         } else {
-      //           element.style.opacity = 'unset';
-      //         }
-      //         clearTimeout(timeout);
-      //       }
-      //     },
-      //     {
-      //       querySelectorAll: true,
-      //     }
-      //   );
-      // }
-
-      // if (cover.isProductPage()) {
-      //   cover.waitFor(
-      //     '[data-list="Complementary Product Recommendation PDP"]',
-      //     async (target) => {
-      //       const element: HTMLStyleElement = target.closest('.Grid-cell');
-
-      //       if (element) {
-      //         element.style.opacity = '0';
-
-      //         const timeout = setTimeout(() => {
-      //           element.style.opacity = 'unset';
-      //         }, 1000);
-
-      //         cover.choose.promises['T84'] = cover.choose.experiment('T84');
-
-      //         if (await cover.choose.promises['T84']) {
-      //           element.remove();
-      //         } else {
-      //           element.style.opacity = 'unset';
-      //         }
-      //         clearTimeout(timeout);
-      //       }
-      //     }
-      //   );
-      // }
     }
-
-    // cover.waitFor(
-    //   '.Swiper-button',
-    //   async (element) => {
-    //     cover.choose.promises['T70'] = cover.choose.experiment('T70');
-
-    //     if (await cover.choose.promises['T70']) {
-    //       element.style.opacity = '1';
-    //     }
-    //   },
-    //   {
-    //     querySelectorAll: true,
-    //   }
-    // );
-
-    // cover.waitFor(
-    //   '.Button.Button--green.Button--medium.Button--full.Button--radius.u-hidden',
-    //   async (button) => {
-    //     const container: HTMLStyleElement = button.closest(
-    //       '.ItemTeaser-button, .ItemInfo-button, .ProductSearch-itemCell'
-    //     );
-
-    //     if (!container) {
-    //       return;
-    //     }
-
-    //     const fieldset: HTMLStyleElement =
-    //       container.querySelector('fieldset.AddToCart');
-    //     const input: HTMLFormElement =
-    //       container.querySelector('.AddToCart-input');
-
-    //     // Search will include quantity on load
-    //     if (input.value !== '0') {
-    //       return;
-    //     }
-
-    //     container.style.opacity = '0';
-
-    //     const timeout = setTimeout(() => {
-    //       container.style.opacity = 'unset';
-    //     }, 1000);
-
-    //     cover.choose.promises['T83'] = cover.choose.experiment('T83');
-
-    //     if (await cover.choose.promises['T83']) {
-    //       button.classList.remove('u-hidden');
-    //       fieldset.classList.add('u-hidden');
-
-    //       button.style.minWidth = '120px';
-    //       button.style.textOverflow = 'unset';
-    //       button.style.paddingLeft = '0';
-    //       button.style.paddingRight = '0';
-
-    //       if (container.classList.contains('ItemInfo-button')) {
-    //         button.style.width = '120px';
-    //       }
-    //     }
-    //     container.style.opacity = 'unset';
-    //     clearTimeout(timeout);
-    //   },
-    //   {
-    //     querySelectorAll: true,
-    //     content: 'Köp',
-    //   }
-    // );
   },
   variant: [],
 };
 
 (() => {
   if (cover.isInternetExplorer) {
-    return;
-  }
-
-  if (cover.abtestFlag() == 'false') {
-    console.log('Experiments disabled');
     return;
   }
 
